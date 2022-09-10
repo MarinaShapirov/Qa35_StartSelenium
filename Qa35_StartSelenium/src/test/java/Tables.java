5d8559445416042cbc93cc272cede36f26021183
MarinaShapirov <marinashapirov@gmail.com>
2022-09-10 18:10:37 +0300

    hw-3

--- Qa35_StartSelenium/src/test/java/Tables.java
+++ Qa35_StartSelenium/src/test/java/Tables.java
@@ -31,12 +31,55 @@ public class Tables {
         List<WebElement> colList = wd.findElements(By.cssSelector("#customers tr:first-child th")); //'#customers th' is the same
         Assert.assertEquals(colList.size(), 3);
         wd.quit();
+    }
+
+    @Test
+    public void homeWorkTable(){
+        wd = new ChromeDriver();
+        wd.navigate().to("file:///C:/Users/marin/Downloads/index.html");
+
+        // print count rows in table
+        //List<WebElement> list = wd.findElements(By.cssSelector("#country-table>tbody>tr"));
+        List<WebElement> list = wd.findElements(By.cssSelector("tr"));
+        System.out.println("Rows Count:" + list.size());
+        Assert.assertEquals(list.size(), 4);
+
+        List<WebElement> list_xph = wd.findElements(By.xpath("//tr"));
+        System.out.println("xPath: Rows Count:" + list_xph.size());
+        Assert.assertEquals(list_xph.size(), 4);
 
 
 
+        //print last row
+        //WebElement lastRow = wd.findElement(By.cssSelector("#country-table>tbody>tr:last-child"));
+        WebElement lastRow = wd.findElement(By.cssSelector("tr:last-child"));
+        System.out.println("Last row:" + lastRow.getText());
+
+        WebElement lastRow_xph = wd.findElement(By.xpath("//tr[last()]"));
+        System.out.println("xPath: Last row:" + lastRow_xph.getText());
 
 
 
+        // print count of columns
+        //List<WebElement> colList = wd.findElements(By.cssSelector("tbody>tr:first-child td"));
+        List<WebElement> colList = wd.findElements(By.cssSelector("tr:first-child td"));
+        System.out.println("Col Count:" + colList.size());
+        Assert.assertEquals(colList.size(), 3);
 
+        List<WebElement> colList_xph = wd.findElements(By.xpath("//tr[1]/td"));
+        System.out.println("xPath: Col Count:" + colList_xph.size());
+        Assert.assertEquals(colList_xph.size(), 3);
+
+
+
+        // print text Israel
+        //WebElement textEl = wd.findElement(By.cssSelector("tbody>tr:nth-child(2)>td:nth-child(2)"));
+        WebElement textEl = wd.findElement(By.cssSelector("tr:nth-child(2)>td:nth-child(2)"));
+        System.out.println("Text:" + textEl.getText());
+
+        WebElement textEl_xph = wd.findElement(By.xpath("//*[text()='Israel']"));
+        System.out.println("xPath: Text:" + textEl_xph.getText());
+
+        wd.quit();
     }
 }
